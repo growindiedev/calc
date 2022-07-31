5591d3bc9d2f2e85d5634b31935d779d8ce83904
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-31 16:48:03 +0530

    add partial logic

--- main.js
+++ main.js
@@ -1,23 +1,71 @@
-function add(a, b) {
-  return a + b;
-}
+let storeNum = 0;
+let storeOp = 0;
+let store = [];
 
-function subtract(a, b) {
-  return a - b;
-}
+let operations = {
+  add: (a, b) => {
+    return a + b;
+  },
 
-function multiply(a, b) {
-  return a * b;
-}
+  subtract: (a, b) => {
+    return a - b;
+  },
 
-function divide(a, b) {
-  return a / b;
-}
+  multiply: (a, b) => {
+    return a * b;
+  },
 
-function power(a, b) {
-  return a ** b;
-}
+  divide: (a, b) => {
+    return a / b;
+  },
 
-function operate(operator, a, b) {
-  return operator(a, b);
-}
+  power: (a, b) => {
+    return a ** b;
+  },
+};
+
+let operate = (a, operator, b) => {
+  let res = operations[operator](Number(a), Number(b));
+  return res;
+};
+
+let screen = document.querySelector(".screen");
+
+document.querySelectorAll("button").forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    //(screen.innerText += e.target.innerText)
+    let value = e.target.value;
+    let isNum = Array.from(btn.classList).includes("num");
+    let isOperation = Array.from(btn.classList).includes("operate");
+    let result = 0;
+
+    if (isNum) {
+      storeNum += value;
+      isNum = false;
+    } else if (isOperation) {
+      storeOp = value;
+      isOperation = false;
+    }
+
+    if (storeNum && storeOp) {
+      let result;
+      store.push(storeNum);
+      store.push(storeOp);
+      if (store.length > 3) {
+        console.log(store[0], store[1], store[2]);
+        result = operate(store[0], store[1], store[2]);
+        if (store[3] == "equal") {
+          //complete this
+          screen.innerText = result;
+          console.log("rio", result);
+          return result;
+        }
+        let newStore = [result, store[3]];
+        store = newStore;
+        console.log("store", store);
+      }
+      storeNum = 0;
+      storeOp = 0;
+    }
+  })
+);
