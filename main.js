9c4f7e1e7d9e21df7b69afce8bd7d5fa3383b33d
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-01 20:21:15 +0530

    refactor code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let storeNum = 0;
+let storeNum = "";
 let storeOp = 0;
 let store = [];
 
@@ -33,17 +33,6 @@ let operate = (a, operator, b) => {
   return res;
 };
 
-// let toPush = (char) => {
-//   let flag = false;
-//   let last = store[store.length - 1];
-//   if(typeof Number(last) === "number" && typeof Number(char) !== "number"){
-//     flag = true;
-//   } else if (typeof Number(last) !== "number" && !Object.keys(operations).includes(last) && !Object.keys(operations).includes(char)){
-//     flag = true;
-//   }
-//   return flag;
-// }
-
 let screen = document.querySelector(".screen");
 
 document.querySelectorAll("button").forEach((btn) =>
@@ -52,11 +41,14 @@ document.querySelectorAll("button").forEach((btn) =>
     let isNum = Array.from(btn.classList).includes("num");
     let isOperation = Array.from(btn.classList).includes("operate");
     let isEqual = Array.from(btn.classList).includes("equal");
-    let result = 0;
 
     if (isNum) {
-      storeNum += value;
-      isNum = false;
+      if (value === "." && storeNum.includes(".")) {
+        isNum = false;
+      } else {
+        storeNum += value;
+        isNum = false;
+      }
     } else if (isOperation) {
       storeOp = value;
       isOperation = false;
@@ -69,46 +61,41 @@ document.querySelectorAll("button").forEach((btn) =>
       return res;
     }
 
-    if (storeNum || storeOp) {
-      let result;
-      // storeNum && store.push(storeNum);
-      if (storeNum && !Number(store[store.length - 1]) && storeOp) {
-        store.push(storeNum);
-        storeNum = 0;
-      }
-
-      if (
-        !Object.keys(operations).includes(store[store.length - 1]) &&
-        storeOp
-      ) {
-        // do not push the storeOp if last array value have same storeOp
-        //here is the problem
-        store.push(storeOp);
-        storeOp = 0;
-      } else if (
-        Object.keys(operations).includes(store[store.length - 1]) &&
-        storeOp
-      ) {
-        store[store.length - 1] = storeOp;
-        storeOp = 0;
-      } else if (!store.length && storeOp) {
-        store.push(0);
-        store.push(storeOp);
-        storeOp = 0;
-      }
-      //store[0] && storeOp && store.push(storeOp);
-      if (store.length > 3) {
-        console.log(store[0], store[1], store[2]);
-        result = operate(store[0], store[1], store[2]);
-        console.log("rio", result);
-        let newStore = [result, store[3]];
-        store = newStore;
-        console.log("store", store);
-        storeNum = 0;
-        storeOp = 0;
-      }
+    if (storeNum && storeOp) {
+      //!Number(store[store.length - 1])
+      store.push(storeNum);
+      storeNum = 0;
     }
-    //console.log("checkStoreNum", storeNum);
+
+    if (!store.length && storeOp) {
+      store.push(0);
+      store.push(storeOp);
+      storeOp = 0;
+    }
+
+    if (!Object.keys(operations).includes(store[store.length - 1]) && storeOp) {
+      // do not push the storeOp if last array value have  storeOp
+      //here is the problem
+      store.push(storeOp);
+      storeOp = 0;
+    } else if (
+      Object.keys(operations).includes(store[store.length - 1]) &&
+      storeOp
+    ) {
+      store[store.length - 1] = storeOp;
+      storeOp = 0;
+    }
+    if (store.length > 3) {
+      console.log(store[0], store[1], store[2]);
+      result = operate(store[0], store[1], store[2]);
+      console.log("res", result);
+      let newStore = [result, store[3]];
+      store = newStore;
+      console.log("store", store);
+      storeNum = 0;
+      storeOp = 0;
+      //}
+    }
     console.log("checkStore", store);
   })
 );
